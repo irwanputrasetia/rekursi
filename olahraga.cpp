5a0dfe9439d2433654640c462ad729da9a64c301
irwanputrasetia <irwanputrastya@gmail.com>
2018-01-18 21:52:02 +0700

    pembuatan variabel dan perincian dalam class teknik

--- olahraga.cpp
+++ olahraga.cpp
@@ -9,4 +9,13 @@ class olahraga {
         string teknik;
         void jmlpemain(int jumlah);
         };
-int main(){ }
+int main(){ 
+	olahraga renang,sepakbola,basket,angkatbesi,lari,voli; 
+	renang.teknik		= " menahan napas, dan bergerak diair ";
+	sepakbola.teknik 	= " menendang bola hingga masuk ke gawang ";
+	basket.teknik 		= " memainkan bola dan melempar ke dalam ring ";
+	angkatbesi.teknik 	= "mengangkat besi dengan beban dan waktu yang ditentukan ";
+	lari.teknik 	    = "berlari cepat hingga garis finish ";
+	voli.teknik  		= "memainkan bola bertim dengan tangan jangan sampai terjatuh ";
+	
+}

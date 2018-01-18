1057299823ac01ecf21a09943df5ca480148c766
irwanputrasetia <irwanputrastya@gmail.com>
2018-01-18 21:54:24 +0700

    pengisian dan pemanggilan masing-masing variable

--- olahraga.cpp
+++ olahraga.cpp
@@ -18,4 +18,45 @@ int main(){
 	lari.teknik 	    = "berlari cepat hingga garis finish ";
 	voli.teknik  		= "memainkan bola bertim dengan tangan jangan sampai terjatuh ";
 	
+	cout << "ciri-ciri renang: \n\n";
+	cout << (renang.teknik);
+	renang.gerak(" mengayuh tangan dan menggerakkan kaki di air ");
+	renang.tempat("kolam ");
+	renang.jmlpemain(1);
+	cout <<"===================================\n\n";
+	
+	cout << "ciri-ciri sepakbola: \n\n";
+	cout << (sepakbola.teknik);
+	sepakbola.gerak("menendang bola menggunakan kaki");
+	sepakbola.tempat("dilapangan luas berumput");
+	sepakbola.jmlpemain(11);
+	cout <<"===================================\n\n";
+	
+	cout << "ciri-ciri basket: \n\n";
+	cout << (basket.teknik);
+	basket.gerak("memantulkan bola menggunakan tangan ");
+	basket.tempat("lapangan dengan ada ring yang bergantung");
+	basket.jmlpemain(5);
+	cout <<"===================================\n\n";
+	
+	cout << "ciri-ciri angkat besi: \n\n";
+	cout << (angkatbesi.teknik);
+	angkatbesi.gerak("mengangkat besi dengan beban");
+	angkatbesi.tempat("gym");
+	angkatbesi.jmlpemain(1);
+	cout <<"===================================\n\n";
+	
+	cout << "ciri-ciri lari: \n\n";
+	cout << (lari.teknik);
+	lari.gerak("berlari sekuat tenaga menggunakan kaki");
+	lari.tempat("lapangan atau tempat terbuka");
+	lari.jmlpemain(1);
+	cout <<"===================================\n\n";
+	
+	cout << "ciri-ciri voli: \n\n";
+	cout << (voli.teknik);
+	voli.gerak("memukul dan mengcontrol bola agar tidak terjatuh");
+	voli.tempat("lapangan berbatas di net ditengah lapangan");
+	angkatbesi.jmlpemain(6);
+	cout <<"===================================\n\n";
 }

c7d70facf1c011aa3fa32e6270b271ed9cf74d1e
irwanputrasetia <irwanputrastya@gmail.com>
2018-01-18 21:50:01 +0700

    awal dan penambahan class beserta tipe datanya

---
+++ olahraga.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class olahraga {
+    public:
+        void gerak(string gerakan);
+        int tempat(string lapangan);
+        string teknik;
+        void jmlpemain(int jumlah);
+        };
+int main(){ }
